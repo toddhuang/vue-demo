df43d3c733b660e52f37174543623b3d84198003
todd.huang <todd.huang.cn@gmail.com>
2024-08-23 17:22:30 +0800

    feat(commit lint): add some rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
+}
